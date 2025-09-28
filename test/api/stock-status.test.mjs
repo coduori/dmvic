93d0097acfcac02fba9f9b3695792d1cda0f11b3
oduori <oduori.oranga@gmail.com>
2025-09-28 18:16:03 +0300

    refactor: update checkStockStatus to use insurer identifier from INSURERS

--- test/api/stock-status.test.mjs
+++ test/api/stock-status.test.mjs
@@ -7,6 +7,8 @@ import {
     mockGetSecret,
     mockSecretsHandler,
 } from '../mocks/mocks.mjs';
+import { INSURERS } from '../../lib/utils/insurers.mjs';
+import { cryptoPickOne } from '../random-pick.mjs';
 
 const mockGetAPIBaseURL = mockApiConfig.getAPIBaseURL;
 
@@ -27,23 +29,27 @@ describe('checkStockStatus', () => {
         );
     });
 
-    it('should call invoke with correct arguments and returns response', async () => {
-        mockInvoke.mockResolvedValue({ stock: 10 });
-        const result = await checkStockStatus('token123', 27);
-        expect(mockGetSecret).toHaveBeenCalledWith('environment');
-        expect(mockGetAPIBaseURL).toHaveBeenCalledWith('test');
-        expect(mockInvoke).toHaveBeenCalledWith(
-            'POST',
-            'https://test-api.example.com/api/t5/Integration/MemberCompanyStock',
-            { MemberCompanyId: 27 },
-            'token123'
-        );
-        expect(result).toEqual({ stock: 10 });
-    });
+    it.each(Object.keys(INSURERS))(
+        'should call invoke with correct arguments and returns response: %s',
+        async (insurer) => {
+            mockInvoke.mockResolvedValue({ stock: 10 });
+            const result = await checkStockStatus('token123', insurer);
+            expect(mockGetSecret).toHaveBeenCalledWith('environment');
+            expect(mockGetAPIBaseURL).toHaveBeenCalledWith('test');
+            expect(mockInvoke).toHaveBeenCalledWith(
+                'POST',
+                'https://test-api.example.com/api/t5/Integration/MemberCompanyStock',
+                { MemberCompanyId: INSURERS[insurer] },
+                'token123'
+            );
+            expect(result).toEqual({ stock: 10 });
+        }
+    );
 
     it('should throw with correct message if invoke throws', async () => {
+        const insurer = cryptoPickOne(Object.keys(INSURERS));
         mockInvoke.mockRejectedValue(new Error('Network error'));
-        await expect(checkStockStatus('token123', 27)).rejects.toThrow(
+        await expect(checkStockStatus('token123', insurer)).rejects.toThrow(
             'Error fetching data: Network error'
         );
     });
