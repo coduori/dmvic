3cf1af0926be39f1b567784c425599cc482173f4
coduori <90685462+coduori@users.noreply.github.com>
2025-09-28 23:59:10 +0300

    fix: update cancel certificate function signature to take an enum of cancellation reasons (#55)
    * fix: update cancel certificate function signature to take an enum of cancellation reasons

--- test/api/cancel-certificate.test.mjs
+++ test/api/cancel-certificate.test.mjs
@@ -6,6 +6,8 @@ import {
     mockRequestHandler,
     mockSecretsHandler,
 } from '../mocks/mocks.mjs';
+import { cryptoPickOne } from '../random-pick.mjs';
+import { CANCELLATION_REASONS } from '../../lib/utils/cancellation-reasons.mjs';
 
 jest.unstable_mockModule('../../lib/utils/request-handler.mjs', () => mockRequestHandler);
 jest.unstable_mockModule('../../lib/config/api-configs.mjs', () => mockApiConfig);
@@ -19,15 +21,42 @@ describe('Cancel certificate issuance', () => {
     });
 
     it('should throw if no authToken is provided', async () => {
-        await expect(cancelCertificate(null, 'CERT123')).rejects.toThrow(
-            'Authentication token is required!'
+        const cancellationReason = cryptoPickOne(Object.keys(CANCELLATION_REASONS));
+        await expect(cancelCertificate(null, 'CERT123', cancellationReason)).rejects.toThrow(
+            /Authentication token is required!/
         );
     });
 
+    it.each([
+        ['empty string', ''],
+        ['whitespace string', ' '],
+        ['invalid string', 'UNSUPPORTED_REASON'],
+        ['invalid data type', true],
+    ])(
+        'should throw if provided reason is not supported: %s - %s',
+        async (description, unsupportedReason) => {
+            const errorString = `${unsupportedReason} is not a valid reason.`;
+            await expect(() =>
+                cancelCertificate('valid-auth-token', 'CERT123', unsupportedReason)
+            ).rejects.toThrow(new RegExp('^' + errorString + '.*'));
+        }
+    );
+
+    it.each(Object.keys(CANCELLATION_REASONS))(
+        'should accept all valid cancellation reasons: %s',
+        async (cancellationReason) => {
+            expect(() =>
+                cancelCertificate('token123', 'C27384993', cancellationReason)
+            ).not.toThrow();
+        }
+    );
+
     it('should call invoke with correct arguments and returns response', async () => {
+        const cancellationReason = cryptoPickOne(Object.keys(CANCELLATION_REASONS));
+
         mockInvoke.mockResolvedValue({
             responseBody: {
-                Inputs: '{"certificatenumber":"C27384993","cancelreasonid":18}',
+                Inputs: `{"certificatenumber":"C27384993","cancelreasonid":${CANCELLATION_REASONS[cancellationReason]}}`,
                 callbackObj: { TransactionReferenceNumber: 'UAT-XAA0552' },
                 success: true,
                 Error: [],
@@ -36,17 +65,22 @@ describe('Cancel certificate issuance', () => {
             },
             statusCode: 200,
         });
-        const result = await cancelCertificate('token123', 'C27384993', 18);
+
+        const result = await cancelCertificate('token123', 'C27384993', cancellationReason);
         expect(mockInvoke).toHaveBeenCalledTimes(1);
+
         expect(mockInvoke).toHaveBeenCalledWith(
             'POST',
             'https://test-api.example.com/api/t5/Integration/CancelCertificate',
-            { CertificateNumber: 'C27384993', cancelreasonid: 18 },
+            {
+                CertificateNumber: 'C27384993',
+                cancelreasonid: CANCELLATION_REASONS[cancellationReason],
+            },
             'token123'
         );
         expect(result).toEqual({
             responseBody: {
-                Inputs: '{"certificatenumber":"C27384993","cancelreasonid":18}',
+                Inputs: `{"certificatenumber":"C27384993","cancelreasonid":${CANCELLATION_REASONS[cancellationReason]}}`,
                 callbackObj: { TransactionReferenceNumber: 'UAT-XAA0552' },
                 success: true,
                 Error: [],
@@ -59,7 +93,9 @@ describe('Cancel certificate issuance', () => {
 
     it('should throw with correct message if invoke throws', async () => {
         mockInvoke.mockRejectedValue(new Error('Network error'));
-        await expect(cancelCertificate('token123', 'CERT123')).rejects.toThrow(
+        const cancellationReason = cryptoPickOne(Object.keys(CANCELLATION_REASONS));
+
+        await expect(cancelCertificate('token123', 'CERT123', cancellationReason)).rejects.toThrow(
             'Error fetching data: Network error'
         );
     });
