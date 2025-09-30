e7e0d5c77bcbb9dcba68c6fb8c5baaf54c3167a7
oduori <oduori.oranga@gmail.com>
2025-09-30 20:24:24 +0300

    chore: update jest linting rules

--- eslint.config.js
+++ eslint.config.js
@@ -119,6 +119,8 @@ export default [
             'jest/no-disabled-tests': 'error',
             'jest/no-focused-tests': 'error',
             'jest/no-identical-title': 'error',
+            'jest/prefer-to-have-length': 'error',
+            'jest/valid-expect': 'error',
 
             'max-lines': 'off',
             'max-lines-per-function': 'off',
